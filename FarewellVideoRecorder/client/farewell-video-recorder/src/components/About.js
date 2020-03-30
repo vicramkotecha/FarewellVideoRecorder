33dd352ec2f41ddd2d76515e381746f28945d597
Samuel Ogden <sam.george.ogden@gmail.com>
2020-03-30 15:25:03 +0100

    feat: add content to about page

--- FarewellVideoRecorder/client/farewell-video-recorder/src/components/About.js
+++ FarewellVideoRecorder/client/farewell-video-recorder/src/components/About.js
@@ -4,5 +4,17 @@ import { Container } from 'react-bootstrap';
 export default () => (
     <Container>
         <h2>About Farewell Memories</h2>
+        <p>
+          Covid 19 has rapidly become a global pandemic. The unfortunate truth is that not everyone makes it out. This is a reality we have to face. This virus can target anyone, but our older population is at a much higher risk. The older population are characteristically less connected to their phone. And due to the nature of the disease this can leave them disconnected from their loved ones. 
+        </p>
+        <p>
+          My name is Vicram Kotecha and together with an amazing team of professionals we have developed Farewell Memories. An app that allows critically ill patients to record a final message to their loved ones, using the phone of a nurse, doctor, or fellow patient.  This can include their wishes and final “I love you’s”.  
+        </p>
+        <p>
+          The carer or patient can launch the app, press “record”, and then provide their name and the contact details of who they’d like to leave the message for.  They record their message and the app will securely upload the video to our server, where volunteers view the contact details and get it to the intended recipients.
+        </p>
+        <p>
+          Farewell Memories plays an important role in bringing peace of mind to the families who were struck by the disease. Nobody should be left alone in such a dire time.
+        </p>
     </Container>
 )
